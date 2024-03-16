12cb6ba089312052f2cb6c6fb4242f03ce3ed161
phoenixrgarcia <phoenixrgarcia@gmail.com>
2024-03-15 21:53:54 -0700

    skeleton of shop.js added

---
+++ shop.js
@@ -0,0 +1,24 @@
+class Shop extends Phaser.Scene
+{
+    
+    constructor ()
+    {
+        super('Shop');
+    }
+
+    init (data)
+    {
+        
+    }
+
+    preload ()
+    {
+        
+    }
+
+    create ()
+    {
+        
+    }
+}
+
