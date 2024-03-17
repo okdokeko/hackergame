736b2ac4a5c4c48b7623089c41e0750c53fea601
phoenixrgarcia <phoenixrgarcia@gmail.com>
2024-03-17 15:02:58 -0700

    changed deck letters from obj to char

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -61,7 +61,6 @@ class Start extends Phaser.Scene {
 }
 
 
-//TODO: verify the code is working and that the deck can be imported/manipulated in scenes
 class Deck {
     
     constructor() {
@@ -101,39 +100,39 @@ class Deck {
     initDeck(){
 
         const letters = [
-            { letter: 'A', value: 1 },
-            { letter: 'B', value: 3 },
-            { letter: 'C', value: 3 },
-            { letter: 'D', value: 2 },
-            { letter: 'E', value: 1 },
-            { letter: 'F', value: 4 },
-            { letter: 'G', value: 2 },
-            { letter: 'H', value: 4 },
-            { letter: 'I', value: 1 },
-            { letter: 'J', value: 8 },
-            { letter: 'K', value: 5 },
-            { letter: 'L', value: 1 },
-            { letter: 'M', value: 3 },
-            { letter: 'N', value: 1 },
-            { letter: 'O', value: 1 },
-            { letter: 'P', value: 3 },
-            { letter: 'Q', value: 10 },
-            { letter: 'R', value: 1 },
-            { letter: 'S', value: 1 },
-            { letter: 'T', value: 1 },
-            { letter: 'U', value: 1 },
-            { letter: 'V', value: 4 },
-            { letter: 'W', value: 4 },
-            { letter: 'X', value: 8 },
-            { letter: 'Y', value: 4 },
-            { letter: 'Z', value: 10 }
+            'A',
+            'B',
+            'C',
+            'D',
+            'E',
+            'F',
+            'G',
+            'H',
+            'I',
+            'J',
+            'K',
+            'L',
+            'M',
+            'N',
+            'O',
+            'P',
+            'Q',
+            'R',
+            'S',
+            'T',
+            'U',
+            'V',
+            'W',
+            'X',
+            'Y',
+            'Z'
         ];
     
-        const initiDeckSize = 10;
+        const initiDeckSize = 40;
         for (let i = 0; i < initiDeckSize; i++) {
             const randomIndex = Math.floor(Math.random() * letters.length);
-            const randomLetterObj = letters[randomIndex]; // Generating random letter object
-            this.addLetter(randomLetterObj); // Adding letter object to the deck
+            const randomLetter = letters[randomIndex]; // Generating random letter object
+            this.addLetter(randomLetter); // Adding letter object to the deck
         }
     }
 }
