b3dbc8250fc2aa663922e996d1425f0e4b4b513d
okdokeko <125633088+okdokeko@users.noreply.github.com>
2024-03-17 01:19:21 -0700

    worked on deck but failed, might have to move to start file

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -1,3 +1,5 @@
+// import Deck from '../Data/Deck.js'; //TODO: Fix import
+
 class Start extends Phaser.Scene {
 
     constructor() {
@@ -12,43 +14,17 @@ class Start extends Phaser.Scene {
 
     }
 
+
+
     create() {
         var data = {
             money: 10, // initial money tbd
             score: 0,
-            deck: {
-                head: null,
-                tail: null,
-                length: 0,
-                addLetter: function(letter) {
-                    const node = {
-                        letter: letter,
-                        next: null
-                    };
-                    if (this.head === null) {
-                        this.head = node;
-                        this.tail = node;
-                    } else {
-                        this.tail.next = node;
-                        this.tail = node;
-                    }
-                    this.length++;
-                },
-                removeLetter: function() {
-                    if (this.head === null) {
-                        return null;
-                    }
-                    const letter = this.head.letter;
-                    this.head = this.head.next;
-                    if (this.head === null) {
-                        this.tail = null;
-                    }
-                    this.length--;
-                    return letter;
-                }
-            },
-            level: 0, //which shop/boss loop is the player? entering shop increments this.
+            // deck: new Deck(), // TODO: fix the deck not loading
+            level: 0,
         }
+
+        // data.deck.initDeck();  // TODO: fix the deck being sussy
 
         // Add a background image
         this.background = this.add.tileSprite(0, 0, config.width, config.height, "startBackground");
