4d9dfc7e315cc74956581fd5fec7d336062159f0
okdokeko <125633088+okdokeko@users.noreply.github.com>
2024-03-17 22:46:08 -0700

    added clear button

--- src/scenes/Boss.js
+++ src/scenes/Boss.js
@@ -23,77 +23,35 @@ class Boss extends Phaser.Scene {
             'j': 2, 'k': 2, 'l': 1, 'm': 2, 'n': 3, 'o': 3, 'p': 2, 'q': 2, 'r': 3,
             's': 3, 't': 3, 'u': 2, 'v': 2, 'w': 2, 'x': 2, 'y': 2, 'z': 1
         };
-
+    
         this.currScore = 0;
         this.currMult = 1;
         this.currWord = "";
-
-
-
-///////////////////////////
-
+    
         // Add a background image
         this.background = this.add.tileSprite(0, 0, 5400, 3400, "bossBackground").setScale(.5);
         this.background.setOrigin(0);
-
+    
         // Calculate the color of the overlay based on the level
         const overlayColor = Phaser.Display.Color.GetColor(255 - this.data.level * 15, 204 - this.data.level * 15, 204 - this.data.level * 15);
-
+    
         // Add a rectangle to act as the overlay
         this.overlay = this.add.rectangle(0, 0, config.width, config.height, overlayColor);
         this.overlay.setOrigin(0);
         this.overlay.setAlpha(.1);
-
-        this.add.particles(config.width * 15 / 100, config.height * 10/100, 'flame', {
-            speed: 200,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        });
-        this.add.particles(config.width * 85 / 100, config.height * 10/100, 'flame', {
-            speed: 200,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        });
-
-        this.add.image(config.width / 2, config.height * 1.3 / 10, "title_holder").setScale(.15, .04)
-
-
+    
         this.bossName = this.getBossNameByLevel(this.data.level);
         const textBox = this.add.bitmapText(config.width / 2, config.height * 1.3 / 10, 'vermin', `${this.bossName}`, 60).setOrigin(.5)
         textBox.setTint(0xB22222)
-        //const textBox = this.add.text(config.width / 2, config.height / 10, `${this.bossName}`, {
-        //    fontFamily: 'Arial',
-        //    fontSize: '24px',
-        //    color: '#ffffff',
-        //    backgroundColor: '#000000',
-        //    padding: { x: 10, y: 5 }
-        //}).setOrigin(0.5);
-
+    
         this.bossImage = this.add.image(config.width * 20 / 100, config.height * 40/ 100, 'boss' + this.data.level);
         this.bossImage.displayWidth = 400;
         this.bossImage.displayHeight = 250;
-
-        // const rect = this.add.rectangle(config.width / 2, config.height / 1.3, config.width / 2, config.height / 3, 0x000000);
-
+    
         const healthBar = this.add.rectangle(config.width * 66/ 100, config.height * 28 / 100, config.width / 3, config.height / 15, 'red');
-
+    
         const hpText = this.add.bitmapText(config.width * 40/100, config.height * 25 / 100, 'vermin', 'HP:', 18).setScale(2);
-        //const hpText = this.add.text(config.width / 2.5, config.height / 6 - 30, "HP", {
-        //    fontFamily: 'Arial',
-        //    fontSize: '18px',
-        //    color: '#ffffff',
-        //    backgroundColor: '#000000',
-        //    padding: { x: 10, y: 5 }
-        //}).setOrigin(0.5);
-
-        // const handText = this.add.text(config.width / 2, config.height / 1.15, "Current Hand", {
-        //     fontFamily: 'Arial',
-        //     fontSize: '24px',
-        //     color: '#ffffff',
-        //     backgroundColor: '#000000',
-        //     padding: { x: 10, y: 5 }
-        // }).setOrigin(0.5);
-
+    
         const playWordText = this.add.text(config.width * 66/ 100, config.height * 62 / 100, "Play Word", {
             fontFamily: 'Arial',
             fontSize: '18px',
@@ -101,36 +59,37 @@ class Boss extends Phaser.Scene {
             backgroundColor: 'gold',
             padding: { x: 10, y: 5 }
         }).setOrigin(0.5);
-
-        const goldLine = this.add.rectangle(healthBar.x, healthBar.y + 220, config.width / 3, 2, 0xffd700);
-        goldLine.setStrokeStyle(4, 0x000000);
-
-        // Add white rectangle behind money text
-        //const moneyBackground = this.add.rectangle(config.width / 9, config.height / 10, 150, 30, 0xffffff);
-        //moneyBackground.setOrigin(0.5);
-
+    
+    
         // Display money
         this.moneyText = this.add.bitmapText(config.width * .1 / 9, config.height * 9.2 / 10,'vermin',`Money: ${this.data.money}`, 25)
-        //this.moneyText = this.add.text(config.width / 9, config.height / 10, `Money: ${this.data.money}`, { font: "25px Arial", fill: "black" });
-        //this.moneyText.setOrigin(0.5);
-
-        // Add white rectangle behind level text
-        //const levelBackground = this.add.rectangle(config.width / 4, config.height / 10, 150, 30, 0xffffff);
-        //levelBackground.setOrigin(0.5);
-
+    
         // Display level
         this.levelText = this.add.bitmapText(config.width * .1 / 9, config.height * 9.6 / 10,'vermin',`Level: ${this.data.level}`, 25)
-        //this.levelText = this.add.text(config.width / 4, config.height / 10, `Level: ${this.data.level}`, { font: "25px Arial", fill: "black" });
-        //this.levelText.setOrigin(0.5);
-
-
-        // Display the player's current hand
-        // this.displayPlayerHand();
-
+    
+        // Clear Word button
+        const clearWordButton = this.add.text(config.width * 66/ 100, config.height * 56 / 100, "Clear Word", {
+            fontFamily: 'Arial',
+            fontSize: '18px',
+            color: 'black',
+            backgroundColor: 'red',
+            padding: { x: 10, y: 5 }
+        }).setOrigin(0.5);
+    
+        // Event handler for Clear Word button
+        clearWordButton.setInteractive();
+        clearWordButton.on('pointerdown', () => {
+            if (this.wordText) {
+                this.currWord = "";
+            }
+        });
+    
+        // Generate cards
         this.generateCardsz(letterScores);
+    
+        // Event handler for Play Word button
         playWordText.setInteractive();
         playWordText.on('pointerdown', () => this.submitCurrentWord());
-    
     }
 
     update() {
@@ -161,6 +120,7 @@ class Boss extends Phaser.Scene {
         }
 
         // Add a text field to display this.currWord
+        this.wordText?.destroy();
         this.wordText = this.add.text(config.width * 66/ 100, config.height * 50 / 100, this.currWord, {
             fontFamily: 'Arial',
             fontSize: '24px',
